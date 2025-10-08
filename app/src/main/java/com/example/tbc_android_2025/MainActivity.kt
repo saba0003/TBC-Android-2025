f427742e5984bc8326d85f63f83e486f7a9bd7e9
Saba Morchilashvili <smorchilashvili@gmail.com>
2025-10-08 06:07:35 +0400

    [WIP] 🚀 add homework-4 solution

--- app/src/main/java/com/example/tbc_android_2025/MainActivity.kt
+++ app/src/main/java/com/example/tbc_android_2025/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.tbc_android_2025
 
 import android.os.Bundle
+import android.util.Log.d
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tbc_android_2025.databinding.ActivityMainBinding
 
@@ -11,5 +12,36 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        d("method", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        d("method", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        d("method", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        d("method", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        d("method", "onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        d("method", "onRestart")
     }
 }
