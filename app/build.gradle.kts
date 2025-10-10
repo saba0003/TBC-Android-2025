8f766d8c821e79ff85f62d0faf19755390c6ec21
Saba Morchilashvili <smorchilashvili@gmail.com>
2025-10-10 05:31:05 +0400

    [WIP] 🚀 add homework-7 solution - 🚑️ hotfix firebase issues by getting rid of it entirely - This version should suffice (for now)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    id("com.google.gms.google-services")
 }
 
 android {
@@ -55,8 +54,6 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(platform(libs.firebase.bom))
-    implementation(libs.firebase.auth.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/tbc_android_2025/RegisterPageActivity.kt
+++ app/src/main/java/com/example/tbc_android_2025/RegisterPageActivity.kt
@@ -5,18 +5,14 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tbc_android_2025.databinding.ActivityRegisterPageBinding
 import com.example.tbc_android_2025.utils.InputsValidator
-import com.google.android.material.snackbar.Snackbar
-import com.google.firebase.auth.FirebaseAuth
 
 class RegisterPageActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegisterPageBinding
-    private lateinit var registry: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState = savedInstanceState)
         binding = ActivityRegisterPageBinding.inflate(layoutInflater)
-        registry = FirebaseAuth.getInstance()
         setContentView(binding.root)
 
         binding.backButton.setOnClickListener {
@@ -33,26 +29,8 @@ class RegisterPageActivity : AppCompatActivity() {
                     password = password
                 )
             ) {
-                registry.createUserWithEmailAndPassword(email, password)
-                    .addOnCompleteListener { task ->
-                        if (task.isSuccessful) {
-                            Snackbar.make(
-                                binding.nextButton,
-                                getString(R.string.successful_registration_label),
-                                Snackbar.LENGTH_SHORT
-                            ).show()
-                            startActivity(Intent(this, LoginPageActivity::class.java))
-                            finish()
-                        } else {
-                            val errorMessage = task.exception?.message
-                                ?: getString(R.string.unsuccessful_registration_label)
-                            Snackbar.make(
-                                binding.nextButton,
-                                errorMessage,
-                                Snackbar.LENGTH_SHORT
-                            ).show()
-                        }
-                    }
+                startActivity(Intent(this, LoginPageActivity::class.java))
+                finish()
             }
         }
     }
