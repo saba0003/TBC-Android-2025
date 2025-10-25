5eefe13e5e5793cc294cec236a3b77707677fc95
Saba Morchilashvili <smorchilashvili@gmail.com>
2025-10-25 18:53:18 +0400

    🔨 update template for homeworks

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,13 +28,16 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
+
     buildFeatures {
         compose = true
         viewBinding = true
