2023779e9307225861d99daf602ec9b4c17b47e3
Saba Morchilashvili <smorchilashvili@gmail.com>
2025-11-05 06:59:21 +0400

    🔨 update template for homeworks

--- app/src/main/java/com/example/tbc_android_2025/commons/BaseFragment.kt
+++ app/src/main/java/com/example/tbc_android_2025/commons/BaseFragment.kt
@@ -7,13 +7,7 @@ import android.view.ViewGroup as Container
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.viewbinding.ViewBinding
-import com.example.tbc_android_2025.R as Resources
 
-typealias Ids = Resources.id
-typealias Strings = Resources.string
-typealias Colors = Resources.color
-typealias Drawables = Resources.drawable
-typealias Images = Resources.mipmap
 typealias ViewBindingInflater<VB> = (LayoutInflater, Container?, Boolean) -> VB
 
 abstract class BaseFragment<VB : ViewBinding>(private val inflater: ViewBindingInflater<VB>) : Fragment() {
---
+++ app/src/main/java/com/example/tbc_android_2025/commons/ResourcesAliases.kt
@@ -0,0 +1,9 @@
+package com.example.tbc_android_2025.commons
+
+import com.example.tbc_android_2025.R as Resources
+
+typealias Ids = Resources.id
+typealias Strings = Resources.string
+typealias Colors = Resources.color
+typealias Drawables = Resources.drawable
+typealias Images = Resources.mipmap
