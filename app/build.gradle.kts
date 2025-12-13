c702e64d8d7b3ece4207eb76004fe824154c8337
Saba Morchilashvili <smorchilashvili@gmail.com>
2025-12-13 17:07:29 +0400

    ➕ add kotlinx date and time dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.kotlinx.datetime)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
