9c4ad2f017c61d196bb7066360347cf3497924f2
Saba Morchilashvili <smorchilashvili@gmail.com>
2026-01-13 02:33:16 +0400

    🔨 update template for homeworks - ➕ add dependency for swipe refresh layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,7 @@ dependencies {
     implementation(libs.kotlinx.datetime)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.core.splashscreen)
+    implementation(libs.androidx.swiperefreshlayout)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.datastore.preferences)
