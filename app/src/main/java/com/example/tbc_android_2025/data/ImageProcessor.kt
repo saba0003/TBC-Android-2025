11aa5ec2d837411cee5b9a2426d7e324b9d6c457
Saba Morchilashvili <smorchilashvili@gmail.com>
2026-01-15 03:10:13 +0400

    🚀 update homework-29 solution

--- app/src/main/java/com/example/tbc_android_2025/data/ImageProcessor.kt
+++ app/src/main/java/com/example/tbc_android_2025/data/ImageProcessor.kt
@@ -18,42 +18,55 @@ import kotlin.uuid.Uuid
 
 class ImageProcessor @Inject constructor(@param:ApplicationContext private val context: Context) {
 
-    @OptIn(ExperimentalUuidApi::class)
     suspend fun process(uri: String): Result<ProcessedPhoto> =
-        withContext(context = Dispatchers.Default) {
-            try {
-                val file = File(
-                    context.cacheDir,
-                    PHOTO_PREFIX.plus(other = Uuid.random()).plus(other = PHOTO_EXTENSION)
-                )
-
-                val inputStream = context.contentResolver.openInputStream(uri.toUri())
-                val bitmap = BitmapFactory.decodeStream(inputStream)
-                inputStream?.close()
-
-                val scaled = bitmap.scale(
-                    width = (bitmap.width * RESOLUTION_SCALE).toInt(),
-                    height = (bitmap.height * RESOLUTION_SCALE).toInt()
-                )
-
-                val format = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
-                    Bitmap.CompressFormat.WEBP_LOSSY
-                else
-                    Bitmap.CompressFormat.WEBP
-
-                FileOutputStream(file).use {
-                    scaled.compress(format, COMPRESSION_QUALITY, it)
-                }
-
-                Result.success(value = ProcessedPhoto(uri = file.toUri().toString()))
-            } catch (e: Exception) {
-                Result.failure(exception = e)
+        withContext(context = Dispatchers.IO) {
+            runCatching {
+                val originalBitmap = decodeUriToBitmap(uri = uri)
+                val scaledBitmap = scaleBitmap(bitmap = originalBitmap)
+                val destinationFile = createTempFile()
+                saveBitmapToFile(bitmap = scaledBitmap, file = destinationFile)
+                ProcessedPhoto(uri = destinationFile.toUri().toString())
             }
         }
+
+
+    /** ========================================== AUX ========================================== */
+    private fun decodeUriToBitmap(uri: String): Bitmap =
+        context.contentResolver.openInputStream(uri.toUri())?.use {
+            BitmapFactory.decodeStream(it)
+        } ?: throw IllegalStateException("Failed to open or decode stream")
+
+    private fun scaleBitmap(bitmap: Bitmap): Bitmap = with(receiver = bitmap) {
+        val newWidth = (width * RESOLUTION_SCALE).toInt()
+        val newHeight = (height * RESOLUTION_SCALE).toInt()
+        return scale(width = newWidth, height = newHeight)
+    }
+
+    @OptIn(ExperimentalUuidApi::class)
+    private fun createTempFile(): File {
+        val directory = File(context.cacheDir, TEMP_DIR_NAME).also {
+            if (!it.exists() && !it.mkdirs())
+                throw IllegalStateException("Failed to create cache directory")
+        }
+        val fileName = PHOTO_PREFIX + Uuid.random() + PHOTO_EXTENSION
+        return File(directory, fileName)
+    }
+
+    private fun saveBitmapToFile(bitmap: Bitmap, file: File) {
+        val format = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
+            Bitmap.CompressFormat.WEBP_LOSSY
+        else
+            Bitmap.CompressFormat.WEBP
+
+        FileOutputStream(file).use { bitmap.compress(format, COMPRESSION_QUALITY, it) }
+    }
+    /** ========================================================================================= */
+
 
     private companion object {
         const val RESOLUTION_SCALE = 0.8
         const val COMPRESSION_QUALITY = 80
+        const val TEMP_DIR_NAME = "processed_images"
         const val PHOTO_PREFIX = "processed_photo_"
         const val PHOTO_EXTENSION = ".webp"
     }
